4c411140c70e68e51fe1f1348c39dfe894159095
SlipperySalmon999 <noelgalan999@gmail.com>
2026-02-19 16:50:57 +1100

    Added PCG code for the plane at this point the plane can be populated with trrees and soliders aat random points of the plane

--- Assets/SpawnEntities.cs
+++ Assets/SpawnEntities.cs
@@ -2,10 +2,36 @@ using UnityEngine;
 
 public class SpawnEntities : MonoBehaviour
 {
+    public GameObject Trees;
+
+    public GameObject Soliders;
+
+    public int TreesToSpawn;
+
+    public int SolidersToSpawn;
+
+    private float XPos;
+
+    private float ZPos;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
+        for (int i = 0; i < TreesToSpawn; i++)
+        {
+            XPos = Random.Range(-100, 100);
+            ZPos = Random.Range(-100, 100);
+            Instantiate(Trees, new Vector3(XPos, 0, ZPos), Quaternion.identity);
+        }
         
+        for (int i = 0; i < SolidersToSpawn; i++)
+        {
+            XPos = Random.Range(-100, 100);
+            ZPos = Random.Range(-100, 100);
+            Instantiate(Soliders, new Vector3(XPos, 0, ZPos), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
