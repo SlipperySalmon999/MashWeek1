63f27103b74ac63cefe18d19d0bb0bb9cf4b24d8
SlipperySalmon999 <noelgalan999@gmail.com>
2026-02-20 22:55:49 +1100

    Made the helicopter xontrols easier to use since realistic is honewastly kinda finicky and honeslty hard for this kinda game.Gonna add mouse rotation controls now

--- Assets/HelicopterControls.cs
+++ Assets/HelicopterControls.cs
@@ -6,32 +6,33 @@ public class HelicopterControls : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody rigidBody;
 
-    [SerializeField] private float Responsiveness;
     [SerializeField] private float Acceleration;
+    [SerializeField] private float Thrust;
+    [SerializeField] public float maxHeight;
 
     private float Throttle;
 
-    private float roll;
-    private float pitch;
-    private float yaw;
+   // private float roll;
+   // private float pitch;
+   // private float yaw;
 
     private void HandleInputs()
     {
-        roll = Input.GetAxis("Roll");
-        pitch = Input.GetAxis("Pitch");
-        yaw = Input.GetAxis("Yaw");
+        //roll = Input.GetAxis("Roll");
+        //pitch = Input.GetAxis("Pitch");
+        //yaw = Input.GetAxis("Yaw");
 
         if (Input.GetKey(KeyCode.Space))
         {
-            Throttle += Time.deltaTime * Acceleration;
+            Throttle += Time.deltaTime * Thrust;
         }
         else if (Input.GetKey(KeyCode.LeftShift))
         {
-            Throttle -= Time.deltaTime * Acceleration;
+            Throttle -= Time.deltaTime * Thrust;
         }
         else
         {
-            Throttle -= Time.deltaTime * Acceleration;
+            Throttle -= Time.deltaTime * Thrust;
         }
         
 
@@ -54,8 +55,38 @@ public class HelicopterControls : MonoBehaviour
     {
         rigidBody.AddForce(transform.up * Throttle, ForceMode.Impulse);
         
+        if (Input.GetKey(KeyCode.W))
+            rigidBody.AddForce(transform.forward * Acceleration, ForceMode.Force);
+
+        if (Input.GetKey(KeyCode.S))
+            rigidBody.AddForce(-transform.forward * Acceleration, ForceMode.Force);
+
+        if (Input.GetKey(KeyCode.A))
+            rigidBody.AddForce(-transform.right * Acceleration, ForceMode.Force);
+
+        if (Input.GetKey(KeyCode.D))
+            rigidBody.AddForce(transform.right * Acceleration, ForceMode.Force);
+        
+
+        if (transform.position.y >= maxHeight)
+        {
+            // Clamp height
+            Vector3 pos = transform.position;
+            pos.y = maxHeight;
+            transform.position = pos;
+
+            // Remove vertical velocity completely
+            rigidBody.linearVelocity = new Vector3(
+                rigidBody.linearVelocity.x,
+                0f,
+                rigidBody.linearVelocity.z
+            );
+        }
+        
+        /*
         rigidBody.AddTorque(transform.right * pitch * Responsiveness);
-        rigidBody.AddTorque(-transform.forward * roll * Responsiveness);
+        //rigidBody.AddTorque(-transform.forward * roll * Responsiveness);
         rigidBody.AddTorque(transform.up * yaw * Responsiveness);
+        */
     }
 }
