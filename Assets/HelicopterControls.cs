292186139c8d9305e0fd41503b0c118927a608e0
SlipperySalmon999 <noelgalan999@gmail.com>
2026-02-20 14:27:08 +1100

    added some code fopr the helicopter gonna delte libraries and restart unities cause there missing dependencies that ive been ignoring up to this point but hoinestly iim gonna play it safe and fix it

---
+++ Assets/HelicopterControls.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class HelicopterControls : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rigidBody;
+
+    [SerializeField] private float Responsiveness;
+    [SerializeField] private float Acceleration;
+
+    private float Throttle;
+
+    private float roll;
+    private float pitch;
+    private float yaw;
+
+    private void HandleInputs()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
