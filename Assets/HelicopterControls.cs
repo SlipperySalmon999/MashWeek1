8d5fd4cc752b5012b88fe38c43e1e17457ae2343
SlipperySalmon999 <noelgalan999@gmail.com>
2026-02-20 22:19:32 +1100

    Helicopter can actually fly properly now gotta add max flight height tho since it kinda flys to infinity

--- Assets/HelicopterControls.cs
+++ Assets/HelicopterControls.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class HelicopterControls : MonoBehaviour
@@ -16,16 +17,45 @@ public class HelicopterControls : MonoBehaviour
 
     private void HandleInputs()
     {
+        roll = Input.GetAxis("Roll");
+        pitch = Input.GetAxis("Pitch");
+        yaw = Input.GetAxis("Yaw");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Throttle += Time.deltaTime * Acceleration;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Throttle -= Time.deltaTime * Acceleration;
+        }
+        else
+        {
+            Throttle -= Time.deltaTime * Acceleration;
+        }
         
+
+        Throttle = Mathf.Clamp(Throttle, 0.0f, 100f);
     }
-    void Start()
+    
+    void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+
+    private void Update()
+    {
+        HandleInputs();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        rigidBody.AddForce(transform.up * Throttle, ForceMode.Impulse);
         
+        rigidBody.AddTorque(transform.right * pitch * Responsiveness);
+        rigidBody.AddTorque(-transform.forward * roll * Responsiveness);
+        rigidBody.AddTorque(transform.up * yaw * Responsiveness);
     }
 }
