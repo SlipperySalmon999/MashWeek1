523c27dd232f8ed37d706ad78c5894710eec132c
SlipperySalmon999 <noelgalan999@gmail.com>
2026-02-22 20:29:39 +1100

    Game should be complete now just gotta polish

--- Assets/SoliderCollision.cs
+++ Assets/SoliderCollision.cs
@@ -4,22 +4,26 @@ public class SoliderCollision : MonoBehaviour
 {
     public AudioClip SoliderSaved;
     private AudioSource audioSource;
+
+    public SpawnEntities DaScript;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameObject goal = GameObject.Find("EndGoal");
-        audioSource = goal.GetComponent<AudioSource>();
+        audioSource = goal.GetComponent<AudioSource>(); 
     }
     
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Goal"))
         {
+            //subtract to the number soliders need to be saved
             Debug.Log("Soldier has been sent to the hosiptal you aer a national hero now :)");
             audioSource.PlayOneShot(SoliderSaved);
+            DaScript.DaWinCondition();
             Destroy(gameObject);
-            
-    
         }
     }
 
--- Assets/SpawnEntities.cs
+++ Assets/SpawnEntities.cs
@@ -10,6 +10,8 @@ public class SpawnEntities : MonoBehaviour
 
     public int SolidersToSpawn;
 
+    private int solidersToSave;
+
     private float XPos;
 
     private float ZPos;
@@ -31,6 +33,18 @@ public class SpawnEntities : MonoBehaviour
             XPos = Random.Range(-100, 100);
             ZPos = Random.Range(-100, 100);
             Instantiate(Soliders, new Vector3(XPos, 0, ZPos), Quaternion.identity);
+        }
+        
+        solidersToSave = SolidersToSpawn;
+    }
+
+    public void DaWinCondition()
+    {
+        solidersToSave--;
+        if (solidersToSave <= 0)
+        {
+            Debug.Log("You're a weener");
+            Application.LoadLevel("Win");
         }
     }
 
